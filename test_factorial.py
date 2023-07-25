3576ce8ab7eb3ec033036af64e56ace6bf5a7ced
Dipesh Reen <dipeshreen@gmail.com>
2023-07-25 11:49:46 -0500

    Removed an edge case

--- test_factorial.py
+++ test_factorial.py
@@ -8,10 +8,6 @@ class TestFactorialFunction(unittest.TestCase):
     def test_factorial_of_positive_number(self):
         self.assertEqual(factorial(5), 120)
 
-    def test_factorial_of_negative_number(self):
-        with self.assertRaises(ValueError):
-            factorial(-3)
-
     def test_factorial_of_large_number(self):
         self.assertEqual(factorial(10), 3628800)
 
